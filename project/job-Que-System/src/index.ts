cf5a3929479c305cc1885db07493e9fb1129a03a
HsrRaza <hr.raza04@gmail.com>
2026-04-02 23:23:10 +0530

    job-quue

--- project/job-Que-System/src/index.ts
+++ project/job-Que-System/src/index.ts
@@ -1,1 +1,11 @@
 console.log("hello");
+// this is a simple job queue system using bullmq and redis
+// we will create a queue and a worker to process the jobs in the queue
+
+import { emailQueue } from "./queues/email.queues.js";
+
+emailQueue.add("sendEmail", {
+    to:"user@example.com",
+    subject:"Hello",
+    body:"This is a test email."
+});
